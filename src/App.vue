ee694a2229783e60b0242b8836ed75203001499c
milaaa <k120240305@gmail.com>
2023-12-30 21:47:28 +0800

    串接 video 視訊畫面

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import { ref, onMounted } from 'vue';
 import gsap from 'gsap';
 const logo = ref(null);
+const myVideo = ref(null);
 onMounted(() => {
   // gsap.to(logo.value, 1, {
   //   y: 200,
@@ -10,15 +11,50 @@ onMounted(() => {
   //   repeat: 2
   // });
 
-  //WITH Timelines
+  //with timelines
   var tl = gsap.timeline({ repeat: 2, repeatDelay: 1 });
   tl.to(logo.value, { x: 100, duration: 1 });
   tl.to(logo.value, { y: 50, duration: 1 });
   tl.to(logo.value, { opacity: 0, duration: 1 });
+
+  async function getMedia(constraints) {
+    let stream = null;
+    try {
+      stream = await navigator.mediaDevices.getUserMedia(constraints);
+      var video = myVideo.value;
+      video.srcObject = stream;
+      video.onloadedmetadata = function () {
+        video.play();
+      };
+      myVideo.value.srcObject = stream;
+    } catch (err) {
+      console.log(err.name + ': ' + err.message);
+    }
+  }
+  var constraints = { audio: true, video: { width: 1280, height: 720 } };
+  getMedia(constraints);
+
+  // promise
+  // var constraints = { audio: true, video: { width: 1280, height: 720 } };
+  // navigator.mediaDevices
+  //   .getUserMedia(constraints)
+  //   .then((mediaStream) => {
+  //     var video = myVideo.value;
+  //     video.srcObject = mediaStream;
+  //     video.onloadedmetadata = function () {
+  //       video.play();
+  //     };
+  //   })
+  //   .catch((err) => {
+  //     console.log(err.name + ': ' + err.message);
+  //   });
 });
 </script>
 
 <template>
+  <div class="video-container">
+    <video src="" ref="myVideo" autoplay="true"></video>
+  </div>
   <img ref="logo" alt="Vue logo" src="./assets/logo.svg" width="125" height="125" />
 </template>
 
@@ -36,5 +72,11 @@ body {
   text-align: center;
   margin-top: 60px;
   background-color: #fff;
+}
+.video-container {
+  display: flex;
+  justify-content: center;
+  height: 450px;
+  overflow: hidden;
 }
 </style>
